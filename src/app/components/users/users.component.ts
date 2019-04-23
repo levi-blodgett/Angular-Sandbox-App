ee5aa65ce237f656b6807f7f77af72e346b80fd7
levi-blodgett <levi_blodgett@outlook.com>
2019-04-23 09:25:46 -0500

    Learned about ngIf and conditionals

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,60 +9,60 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() { }
 
   // Lifecycle method, runs automatically when component is intitialized, like contstructor
   ngOnInit() {
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 30,
-        address: {
-           street: '50 Main St.',
-           city: 'Sioux Falls',
-           state: 'SD'
-        }
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Smith',
-        age: 20,
-        address: {
-           street: '20 School St.',
-           city: 'New York',
-           state: 'NY'
-        }
-      },
-      {
-        firstName: 'Karen',
-        lastName: 'Williams',
-        age: 76,
-        address: {
-           street: '55 Mill St.',
-           city: 'Miami',
-           state: 'FL'
-        }
-      },
-    ];
+    // setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+             street: '50 Main St.',
+             city: 'Sioux Falls',
+             state: 'SD'
+          }
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Smith',
+          age: 20,
+          address: {
+             street: '20 School St.',
+             city: 'New York',
+             state: 'NY'
+          }
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 76,
+          address: {
+             street: '55 Mill St.',
+             city: 'Miami',
+             state: 'FL'
+          }
+        },
+      ];
+
+      this.loaded = true;
+    // }, 2000);
 
     this.addUser(
       {
         firstName: 'David',
-        lastName: 'Jackson',
-        age: 47,
-        address: {
-          street: '12 Wakeson St.',
-          city: 'Boston',
-          state: 'MA'
-        }
+        lastName: 'Jackson'
       }
     );
-  }
+  };
 
   addUser(user: User) {
     this.users.push(user);
-  }
+  };
 
 }
