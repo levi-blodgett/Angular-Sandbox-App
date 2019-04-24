b2c01f203d1ae7c0f32f28ae13f6a7107515e2ea
levi-blodgett <levi_blodgett@outlook.com>
2019-04-24 09:25:05 -0500

    Practiced class binding/ngClass

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,9 +9,10 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
-  showExtended: boolean = true;
+  showExtended: boolean = false;
   loaded: boolean = false;
-  enableAdd: boolean = false;
+  enableAdd: boolean = true;
+  currentClasses = {};
 
   constructor() { }
 
@@ -22,36 +23,39 @@ export class UsersComponent implements OnInit {
         {
           firstName: 'John',
           lastName: 'Doe',
-          age: 30,
+          age: 80,
           address: {
              street: '50 Main St.',
              city: 'Sioux Falls',
              state: 'SD'
           },
           // lorempixel is a site for generating images, height/width/category/number
-          image: 'http://lorempixel.com/600/600/people/2'
+          image: 'http://lorempixel.com/600/600/people/3',
+          isActive: true
         },
         {
           firstName: 'Kevin',
           lastName: 'Smith',
-          age: 20,
+          age: 36,
           address: {
              street: '20 School St.',
              city: 'New York',
              state: 'NY'
           },
-          image: 'http://lorempixel.com/600/600/people/3'
+          image: 'http://lorempixel.com/600/600/people/5',
+          isActive: false
         },
         {
           firstName: 'Karen',
           lastName: 'Williams',
-          age: 76,
+          age: 29,
           address: {
              street: '55 Mill St.',
              city: 'Miami',
              state: 'FL'
           },
-          image: 'http://lorempixel.com/600/600/people/4'
+          image: 'http://lorempixel.com/600/600/people/1',
+          isActive: false
         },
       ];
 
@@ -64,10 +68,21 @@ export class UsersComponent implements OnInit {
         lastName: 'Jackson'
       }
     );
+
+    this.setCurrentClasses();
   };
 
   addUser(user: User) {
     this.users.push(user);
   };
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      // If enableAdd is true, add btn-success class
+      'btn-success': this.enableAdd,
+      'btn-light': this.enableAdd === false,
+      'big-text': this.showExtended
+    }
+  }
 
 }
